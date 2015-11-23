1e0bc76b0bf0e42142b57f6a9fe49a0e82de1da9
Mhindupur Mhindupur <manjunath@mhindupurs-macbook-pro.local>
2015-11-23 22:50:22 +0530

    Initial Commit

---
+++ ratingController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ratingController
+//
+//  Created by manjunath Hindupur on 23/11/15.
+//  Copyright © 2015 manjunath Hindupur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
